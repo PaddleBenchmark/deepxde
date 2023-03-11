4fd38ccdf66e76bacad5c29d93021b16a612a8e2
wangruting <wangruting@baidu.com>
2023-03-11 05:12:53 +0000

    add install paddle

--- test_tipc/dynamic/Navier_Stokes_inverse/benchmark_common/prepare.sh
+++ test_tipc/dynamic/Navier_Stokes_inverse/benchmark_common/prepare.sh
@@ -1,2 +1,3 @@
-pip install -r requirements.txt
+python3.7 -m pip install paddlepaddle-gpu==0.0.0.post112 -f https://www.paddlepaddle.org.cn/whl/linux/gpu/develop.html
+pip3.7 install -r requirements.txt
 export PYTHONPATH=${ROOT_DIR}/deepxde

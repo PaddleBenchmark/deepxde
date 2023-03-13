403d7de65526e8c7194751ce11b7d70888e824e5
wangruting <wangruting@baidu.com>
2023-03-13 06:50:12 +0000

    delete paddle install

--- test_tipc/dynamic/Eular_beam/benchmark_common/prepare.sh
+++ test_tipc/dynamic/Eular_beam/benchmark_common/prepare.sh
@@ -1,3 +1,2 @@
-python3.7 -m pip install paddlepaddle-gpu==0.0.0.post112 -f https://www.paddlepaddle.org.cn/whl/linux/gpu/develop.html
 pip3.7 install -r requirements.txt
 export PYTHONPATH=${ROOT_DIR}/deepxde
